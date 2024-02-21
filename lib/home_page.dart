ad426f7fd80f6248452b96b78196a82357d406b6
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-21 19:28:18 +0530

    Building

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,7 +7,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(child: Text("Shoeperb", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w700),)),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [Padding(
+                padding: EdgeInsets.all(20.0),
+                child: Text("Shoes\nCollection", style: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.bold, fontSize: 35),),
+              ),
+              Expanded(child: 
+              TextField(
+                
+              ),
+              )
+            ]),
+          ],
+        ),
+      ),
     );
   }
 }
