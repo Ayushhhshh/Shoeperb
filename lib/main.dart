325654ec6012930da863191328875cad32b62b4a
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-02-28 22:00:34 +0530

    Building

--- lib/main.dart
+++ lib/main.dart
@@ -23,10 +23,10 @@ class MyApp extends StatelessWidget {
           hintStyle: TextStyle(fontFamily: 'Lato', fontWeight: FontWeight.w700,fontSize: 16)
         )
       ),
-      home: 
-      ProductDetail(
-        product: products[0]
-        ),
+      home: HomePage()
+      // ProductDetail(
+      //   product: products[0]
+      //   ),
     
    );
   }
