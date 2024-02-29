d6b2ed08d0e6ecabc1ec548e01f5a3d17ebfb668
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-01 00:10:46 +0530

    Building

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,6 +1,5 @@
 import 'package:e_commerce/global_var.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -10,9 +9,7 @@ class CartPage extends StatelessWidget {
     return  Scaffold(
       body: ListView.builder(
         itemCount: cart.length,
-        itemBuilder:(context,index) {
-          
-        })
+        itemBuilder:(context,index) {})
     );
   }
 }
