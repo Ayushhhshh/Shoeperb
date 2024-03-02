79e56f02c4ce0ce0f683b465a22f71d02934d6ab
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-02 11:34:19 +0530

    Building

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
@@ -12,8 +13,17 @@ class CartPage extends StatelessWidget {
         title:
         const Text("Cart", style: TextStyle(color: Colors.black),),
         ),
-      body: const Center(child: Text("Cart", style: TextStyle(fontSize: 25, fontFamily: "Lato", fontWeight: FontWeight.bold),)),
-      
+      body: 
+      // const Center(child: Text("Cart", style: TextStyle(fontSize: 25, fontFamily: "Lato", fontWeight: FontWeight.bold),)),
+      const Card(
+        shape: RoundedRectangleBorder(
+        ),
+        color: Colors.black87,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("Ayush", style:TextStyle(color: Colors.white),),
+        ),
+      )
     );
   }
 }
