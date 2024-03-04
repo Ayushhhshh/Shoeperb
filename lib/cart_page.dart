a53791c7f998a957aa7f80f8618e51dc7d88ef50
ayushhhshh <2022bcacsbayush12896@poornima.edu.in>
2024-03-05 00:51:42 +0530

    Building

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -1,11 +1,14 @@
-import 'package:e_commerce/global_var.dart';
+import 'package:e_commerce/cart_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    print(Provider.of<CartProvider>(context).cart);
+    final cart = Provider.of<CartProvider>(context).cart;
     return  Scaffold(
       appBar: AppBar(
         elevation: 1,
